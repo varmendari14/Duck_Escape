56c0f6d7fcdbf8e6b82ad79b597b11556c16188b
Jacob Cianci <35444100+jcianci3@users.noreply.github.com>
2019-10-03 18:40:30 -0400

    started gui
    just some changes and scripts to use in gui.

---
+++ Assets/Scripts/TimeCountdown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountdown : MonoBehaviour
+{
+    public Text timeText;
+    public int countdown;
+
+
+    void Start() {
+    	//countdown = 60; //set starting time here, i just did 60 by default
+    }
+
+    void Update() {
+        //time countdown here. every second passing, do countdown--;
+        timeText.text = "Time Left: " + countdown;
+    }
+}
