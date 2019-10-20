2ee3e9b4da14e720b9e404e8da93006caed41ab0
rshiddapur <rshiddapur@gmail.com>
2019-10-20 01:08:42 -0400

    [ch71] Final additions to title screen - level select has no current functionality

---
+++ Assets/Scripts/ControlsListDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsListDisplay : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject controlsMenu;
+
+    public void BacktoMenu()
+    {
+      mainMenu.SetActive(true);
+      controlsMenu.SetActive(false);
+    }
+
+    public void showControls()
+    {
+      mainMenu.SetActive(false);
+      controlsMenu.SetActive(true);
+    }
+}
