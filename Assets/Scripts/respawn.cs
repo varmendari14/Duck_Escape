8e9881cedaeef71953cbe3c5b3a1c16fbd3a7b44
alexandrafoote <alexandravfoote@gmail.com>
2019-11-18 23:10:45 -0500

    player respawn
    player respawnsand decrease health or dies when falls through water, depending on curr health level

---
+++ Assets/Scripts/respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    public Transform Player;
+    public Transform respawnPoint;
+    public HealthBar healthBar;
+    public int damageAmount = 5;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = respawnPoint.transform.position;
+        healthBar.Damage(damageAmount);
+    }
+}
